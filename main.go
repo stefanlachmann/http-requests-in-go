df9c8024de9c53f3ae76faa9ceb288f5e90079bf
Stefan Lachmann <--local>
2022-05-12 15:10:10 +0200

    make req execeed timelimit and exit on 1

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ func main() {
 			fmt.Printf("server: request body: %s\n", reqBody)
 
 			fmt.Fprintf(w, `{"message": "hello!"}`)
+			time.Sleep(35 * time.Second)
 		})
 		server := http.Server{
 			Addr:    fmt.Sprintf(":%d", serverPort),
@@ -53,9 +54,15 @@ func main() {
 	if err != nil {
 		fmt.Printf("client: could not create request: %s\n", err)
 		os.Exit(1)
+
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{
+		Timeout: 30 * time.Second,
 	}
 
-	res, err := http.DefaultClient.Do(req)
+	res, err := client.Do(req)
 	if err != nil {
 		fmt.Printf("error making http request: %s\n", err)
 		os.Exit(1)
