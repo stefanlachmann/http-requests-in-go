da0476bd7d86acce73ab56ed7fa21cfdd3a619ab
Stefan Lachmann <--local>
2022-05-12 15:06:10 +0200

    make a post

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -15,6 +18,20 @@ func main() {
 		mux := http.NewServeMux()
 		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			fmt.Printf("server: %s /\n", r.Method)
+			fmt.Printf("server: query id: %s\n", r.URL.Query().Get("id"))
+			fmt.Printf("server: content-type: %s\n", r.Header.Get("content-type"))
+			fmt.Printf("server: headers:\n")
+			for headerName, headerValue := range r.Header {
+				fmt.Printf("\t%s = %s\n", headerName, strings.Join(headerValue, ", "))
+			}
+
+			reqBody, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				fmt.Printf("server: could not read request body: %s\n", err)
+			}
+			fmt.Printf("server: request body: %s\n", reqBody)
+
+			fmt.Fprintf(w, `{"message": "hello!"}`)
 		})
 		server := http.Server{
 			Addr:    fmt.Sprintf(":%d", serverPort),
@@ -28,9 +45,17 @@ func main() {
 	}()
 
 	time.Sleep(100 * time.Millisecond)
+	jsonBody := []byte(`{"client_message": "hello, server!"}`)
+	bodyReader := bytes.NewReader(jsonBody)
 
-	requestURL := fmt.Sprintf("http://localhost:%d", serverPort)
-	res, err := http.Get(requestURL)
+	requestURL := fmt.Sprintf("http://localhost:%d?id=1234", serverPort)
+	req, err := http.NewRequest(http.MethodPost, requestURL, bodyReader)
+	if err != nil {
+		fmt.Printf("client: could not create request: %s\n", err)
+		os.Exit(1)
+	}
+
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Printf("error making http request: %s\n", err)
 		os.Exit(1)
@@ -38,4 +63,10 @@ func main() {
 
 	fmt.Printf("client: got response!\n")
 	fmt.Printf("client: status code: %d\n", res.StatusCode)
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("client: could not read response body: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("client: response body: %s\n", resBody)
 }
